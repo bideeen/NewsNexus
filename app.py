faf7a0023b80d173cc0e1e9a9da86dcc5a121fcc
Abideen Bello <b.abideen@yahoo.com>
2023-05-09 05:57:13 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ def get_news():
     return render_template("news.html", articles=summary, yoruba=yo_sentence)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
